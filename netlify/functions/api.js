2381b72b0bf6babdd48bb11e15f5acae900c5c59
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-05-16 16:24:47 -0700

    added openai

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,6 +4,43 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const { Configuration, OpenAIApi } = require('openai');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const configuration = new Configuration({
+  organization: 'org-Afl5yl2Yr3FiTJMEqEHM1tKn',
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+//Create new record
+router.post('/generate-cover-letter', async (req, res) => {
+  console.log('POST /generate-cover-letter');
+
+  const { companyName, yourName, position, jobRequirements, yourExperience } =
+    req.body;
+
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    // model: 'gpt-4',
+    messages: [
+      {
+        role: 'user',
+        content: `Hi, please make me a cover letter for the position of ${position} at ${companyName}. Here is the job requirements: ${jobRequirements}. Here is my relevant experience: ${yourExperience}. And my name is ${yourName}`,
+      },
+    ],
+  });
+
+  res.json({
+    completion: completion.data.choices[0].message,
+  });
+});
 
 let records = [];
 
